5c8b9e8df668d446685d21fd2fa4ccdc92781855
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-16 15:31:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,15 @@
 class Dog 
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end
   
-  @this_dogs_name = Dog(name)
+  def name
+    this_dogs_name
+  end 
   
 end 
 
+lassie = Dog.new 
+lassie.name = "Lassie"
+
+lassie.name 
